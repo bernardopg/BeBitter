1b9ce10c0554eef45fae6ce050a0a434cc9f2976
Bernardo Gomes <bernardo.gomes@bebitterbebetter.com.br>
2025-09-02 11:13:43 -0300

    remove capacitor configuration file

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.bebitter',
-  appName: 'BeBitter',
-  webDir: 'dist'
-};
-
-export default config;
